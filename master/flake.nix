813fc42509a7473a0e0cea1c0429a954bf5d054e
hugosenari <hugosenari@gmail.com>
2022-03-12 03:36:04 -0300

    chore: re index tim_sort

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A new awesome nimble package'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-tim_sort-master.flake = false;
+  inputs.src-tim_sort-master.owner = "bung87";
+  inputs.src-tim_sort-master.ref   = "refs/heads/master";
+  inputs.src-tim_sort-master.repo  = "tim_sort";
+  inputs.src-tim_sort-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-tim_sort-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-tim_sort-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
